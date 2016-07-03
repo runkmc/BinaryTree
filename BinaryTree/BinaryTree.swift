cc4f60c7f2ba5758363dec662f103072c7483946
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-07-02 22:15:29 -0700

    Create BinaryNode

--- BinaryTree/BinaryTree.swift
+++ BinaryTree/BinaryTree.swift
@@ -1,3 +1,6 @@
 import Foundation
 
-
+indirect enum BinaryNode<T> {
+    case Node(BinaryNode<T>, T, BinaryNode<T>)
+    case Empty
+}
--- BinaryTree/BinaryTreeTest.swift
+++ BinaryTree/BinaryTreeTest.swift
@@ -8,3 +8,7 @@
 
 import Foundation
 import XCTest
+
+class BinaryTreeTest: XCTestCase {
+    
+}
